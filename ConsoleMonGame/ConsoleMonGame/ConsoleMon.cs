f5c7e39f1dffd8720a160cb2b8d09bfe569ae2c2
orbitmiyuki <35809@ma-web.nl>
2023-05-12 10:54:09 +0200

    m

---
+++ ConsoleMonGame/ConsoleMonGame/ConsoleMon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMonGame
+{
+    internal class ConsoleMon
+    {
+        internal int health;
+        internal int energy;
+        internal string name;
+    }
+
+    internal class TakeDamage
+    {
+        internal int damage;
+    }
+}
--- ConsoleMonGame/ConsoleMonGame/Program.cs
+++ ConsoleMonGame/ConsoleMonGame/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void TestConsoleMonFunctions()
+{
+    Console.WriteLine("TestConsoleMonFunctions");
+    ConsoleMon mon = new ConsoleMon();
+    mon.TakeDamage(100);
+    mon.DepleteEnergy(20);
+
+    Console.WriteLine(mon.health == -100);
+
+    Console.WriteLine(mon.energy == -20);
+}
